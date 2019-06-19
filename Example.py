da66bb7f476990002e343bb03b09538894a3039d
Steven Hayes <32312887+stevenhayes97@users.noreply.github.com>
2019-06-19 09:38:58 -0500

    Create Example.py

---
+++ Example.py
@@ -0,0 +1,25 @@
+from pyspark.sql.types import StringType
+from pyspark.sql import Row
+
+def my_function(x):
+  return x + "test"
+
+
+# Python Name - myPythonUDF
+# SQL Name - nameForSQLUDF
+# The original funciton name - my_function
+# The function's return type - StringType()
+
+myPythonUDF = spark.udf.register("nameForSQLUDF", my_function, StringType())
+
+# Create a test dataframe
+
+data = spark.createDataFrame([Row(test1='xxxxx', test2='yyyyyy'),
+ Row(test1='asdfasdf', test2='dasffaac'),
+ Row(test1='jjjfjf', test2='fffee3'),
+ Row(test1='xzsef4', test2='farq2345w')])
+
+UDFwithDF = data.select("*", myPythonUDF("test1").alias("test1WithUDF"))
+
+# Displays the dataframe in Databricks where I tested this example
+display(UDFwithDF)
